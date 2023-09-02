e8a861fd1d0c153f5c99ba18ff97a54d6a31e2ba
Janine Sebastian <janinems42@gmail.com>
2023-09-01 20:34:51 -0400

    can now reel in fish

--- src/App.js
+++ src/App.js
@@ -15,14 +15,18 @@ import InputBox from "./InputBox/InputBox.js";
 
 function App() {
   let locations = require('./0data/locations.json').locations;
+  let fishes = require('./0data/fish.json').fish;
 
   const [displayMode, setDisplay] = useState(0);
   const [dayNumber, setDay] = useState(0);
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState({});
   const [flavorText, setFlavor] = useState("");
-  const [visual, setVisual] = useState({image: "", alt: ""});
+  const [visual, setVisual] = useState({});
   const [newLine, setLine] = useState(false);
   const [log, setLog] = useState([<li></li>,<li></li>,<li></li>,<li></li>,<li></li>,<li></li>]);
+  const [inputMode, setInput] = useState("new-day");
+  const [fishBite, setBite] = useState(false);
+  const [timerId, setTimer] = useState(0);
 
   function addLine(l) {
     const temp = log;
@@ -41,16 +45,54 @@ function App() {
     addLine("started day "+dayNumber+"!");
   }
 
-  function changeLocation(loc) {
-    setLocation(locations[loc].locationName);
-    setVisual({image: locations[loc].locationImage, alt: locations[loc].locationImageAlt});
-    if (loc !== 0)
-      addLine("travelled to "+locations[loc].locationName+".");
-    setFlavor(<p>{locations[loc].locationFlavor}</p>);
+  function changeFlavor(t) {
+    setFlavor(<p>{t}</p>);
   }
 
+  function changeLocation(loc) {
+    setLocation(locations[loc]);
+    setVisual({image: locations[loc].locationImage, alt: locations[loc].locationImageAlt});
+    if (loc !== 1 && loc !== location.locationId)
+      addLine("travelled to "+locations[loc].locationName+".");
+    changeFlavor(locations[loc].locationFlavor);
+  }
+
+  function biteEvent() {
+    changeFlavor("you got a bite!!");
+    setBite(true);
+    setTimer(setTimeout(() => {setBite(false); changeFlavor("the fish got away..."); setInput("continue");}, 2000));
+  }
+
+  function castLine() {
+    const waitTime = Math.floor(((Math.random() * location.fishWait) + 2) * 1000);
+    changeFlavor("you're waiting for a bite...");
+    setTimer(setTimeout(biteEvent, waitTime));
+  }
+
+
+  function reelIn() {
+    if(fishBite){
+      clearTimeout(timerId);
+      const fishGacha = Math.random();
+      let caughtFish = 0;
+      location.locationFish.forEach(f => {
+        if (f.chance > fishGacha){
+          caughtFish = f.fish;
+        }
+      });
+      changeFlavor("you reeled in a "+fishes[caughtFish].fishName+"!");
+      setInput("continue");
+      setBite(false);
+    } else {
+      clearTimeout(timerId);
+      setInput("continue");
+      changeFlavor("you reeled in too quickly! you have no fish...");
+    }
+  }
+
+
   useEffect(() => {
-    changeLocation(0);
+    changeLocation(1);
   }, []);
 
   return (
@@ -61,7 +103,7 @@ function App() {
       <div className='page-body'>
 
         <div className='textstuff section'>
-          <TextBox dayNumber={dayNumber} location={location} flavorText={flavorText} />
+          <TextBox dayNumber={dayNumber} location={location.locationName} flavorText={flavorText} />
           <ActionLog log={log} />
         </div>
 
@@ -69,7 +111,7 @@ function App() {
         {displayMode === 0 && <>
           <div className='gamestack'>
             <DisplayBox visual={visual} />
-            <InputBox  changeLocation={changeLocation} />
+            <InputBox  changeLocation={changeLocation} inputMode={inputMode} setInput={setInput} castLine={castLine} reelIn={reelIn} />
           </div>
           <Inventory/>
           </>
--- src/InputBox/InputBox.js
+++ src/InputBox/InputBox.js
@@ -3,17 +3,23 @@ import './InputBox.css';
 
 function InputBox(props) {
 
-  const [inputMode, setInput] = useState(0);
-
     return (
       <div className="rounded noselect input-body">
-        {inputMode === 0 && // 0 = wake-up
+        {props.inputMode === "new-day" && 
         <> 
-          <button className="act-button" onClick={()=> {props.changeLocation(1); setInput(1)}} >go fishing</button>
+          <button className="act-button" onClick={()=> {props.changeLocation(2); props.setInput("at-water")}} >go fishing</button>
         </>}
-        {inputMode === 1 && // 1 = go fish
+        {props.inputMode === "at-water" && 
         <> 
-          <button className="act-button" onClick={()=> {props.changeLocation(1);}} >cast a line</button>
+          <button className="act-button" onClick={()=> {props.castLine(); props.setInput("fishing")}} >cast a line</button>
+        </>}
+        {props.inputMode === "fishing" && 
+        <> 
+          <button className="act-button" onClick={()=> {props.reelIn();}} >reel in</button>
+        </>}
+        {props.inputMode === "continue" && 
+        <> 
+          <button className="act-button" onClick={()=> {props.changeLocation(2); props.setInput("at-water")}}>continue fishing</button>
         </>}
         
       </div>
